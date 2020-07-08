2323a43eebc74ab766760516d099722dbcad389c
bchooxg <bchooxg@github.com>
2020-07-08 16:21:20 +0800

    Created App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const [age, setAge] = useState();
+  const [nw, setNw] = useState(0);
+
+  const date = new Date();
+  const future = new Date();
+
+  future.setFullYear(future.getFullYear() + 30 - age);
+
+  function handleChange(e) {
+    if (e.target.name === 'age') {
+      if (e.target.value < 31) {
+        setAge(e.target.value);
+      }
+    }
+    if (e.target.name === 'nw') {
+      setNw(e.target.value);
+    }
+  }
+
+  console.log(nw);
+
+  const yearsToGo = 30 - age;
+  const monthsToGo = (30 - age) * 12;
+  const yrlyIncome = Math.round((1000000 - nw) / yearsToGo).toLocaleString();
+  const mthlyIncome = Math.round((1000000 - nw) / monthsToGo).toLocaleString();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='box'>
+      {age ? (
+        <h1>To be a millionaire by 30</h1>
+      ) : (
+        <h1>Enter your current age</h1>
+      )}
+
+      {age && <h4>{`Years to go: ${yearsToGo}`}</h4>}
+      {age && <p>{`Yearly income needed: $${yrlyIncome}`}</p>}
+      {age && <h4>{`Months to go: ${monthsToGo}`}</h4>}
+      {age && <p>{`Monthly Income Needed: $${mthlyIncome}`}</p>}
+
+      <input
+        placeholder='Current Age'
+        name='age'
+        type='text'
+        value={age}
+        onChange={handleChange}
+      />
+
+      <br></br>
+
+      {age && (
+        <input
+          placeholder='Current Assets'
+          name='nw'
+          type='text'
+          value={nw}
+          onChange={handleChange}
+        />
+      )}
     </div>
   );
 }
